57b63a028cc204ceba337d452074eee84e0a343e
Musashi Mori <musasi01225@icloud.com>
2018-11-06 22:51:32 +0900

    Screen transition from title to quiz

---
+++ app/src/main/java/com/example/kitep03/quiz/TitleActivity.java
@@ -0,0 +1,22 @@
+package com.example.kitep03.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+
+    public void OnButtonClick(View view){
+        Intent intent = new Intent(this,QuizActivity.class);
+        startActivity(intent);
+    }
+
+
+}
